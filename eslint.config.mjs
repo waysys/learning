d76138387a7fa7bf9f410be82efa24a651a20dc8
Waysys <wshaffer@waysysweb.com>
2024-06-17 21:15:23 -0400

    Initial setup with ESLint

---
+++ eslint.config.mjs
@@ -0,0 +1,17 @@
+import globals from "globals";
+import pluginJs from "@eslint/js";
+
+
+export default [
+  {
+    files: ["**/*.js"], 
+    languageOptions: {
+      sourceType: "module"
+    }
+  },
+  {
+    languageOptions: { 
+      globals: {...globals.browser, ...globals.node} }
+  },
+  pluginJs.configs.recommended,
+];
---
+++ src/app.js
@@ -0,0 +1,16 @@
+const aname = "James"
+
+const person = { first: aname }
+
+console.log(person)
+
+const sayHelloLinting = (fName) => {
+    console.log(`Hello linting, ${fName}`);
+};
+
+sayHelloLinting(aname)
+
+
+
+
+
